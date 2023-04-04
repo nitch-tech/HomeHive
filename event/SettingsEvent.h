70a18367cb18c2e7cfe140a8ab25f9f4565d506c
Nick Barrie <nbarrie2@uwo.ca>
2023-04-03 22:16:00 -0400

    too many comments

--- event/SettingsEvent.h
+++ event/SettingsEvent.h
@@ -1,24 +1,68 @@
-//
-// Created by Nick on Mar 28th 
-//
+/**
+* @file SettingsEvent.h
+* @brief Header file for the SettingsEvent class.
+* This file contains the declaration of the SettingsEvent class, which is responsible
+* for updating the HomeView and subscribing to its events.
+* @author Nick
+* @date March 28th
+*/
 #ifndef SETTINGSEVENT_H
 #define SETTINGSEVENT_H
 
 #include <iostream>
 #include <string>
 #include "../gui/HomeView.h"
+/**
+
+* @class SettingsEvent
+* @brief A class for handling events related to the settings.
+* The SettingsEvent class is responsible for updating the HomeView and subscribing to
+* its events. It contains a static instance of itself and a static pointer to a HomeView
+* object that it subscribes to.
+*/
 class SettingsEvent{
-    
+ 
     public:
+       /**
+    * @brief Get the singleton instance of the SettingsEvent class.
+    * @return A pointer to the singleton instance of the SettingsEvent class.
+    */
         static SettingsEvent* getInstance();
-        void update_main();// call update label in homeview
+    /**
+    * @brief Update the main label in the HomeView.
+    *
+    * This function calls the updateLabel() function in the HomeView class to
+    * update the main label with the latest settings.
+    */
+        void update_main();
+        
+    /**
+    * @brief Subscribe to a HomeView object.
+    *
+    * This function sets the static pointer to the HomeView object that the
+    * SettingsEvent class subscribes to.
+    *
+    * @param view A pointer to the HomeView object to subscribe to.
+    */
         void subscribe(HomeView* view);
 
     private:
+    /**
+    * @brief Private constructor for the SettingsEvent class.
+    *
+    * The constructor is made private to ensure that only one instance of the
+    * SettingsEvent class can be created.
+    */
         SettingsEvent();
+    /**
+      * @brief Private destructor for the SettingsEvent class.
+      *
+      * The destructor is made private to ensure that the SettingsEvent object
+      * can only be destroyed through the getInstance() function.
+      */
         ~SettingsEvent();
-        static SettingsEvent* eventptr;
-        static HomeView* view;
+     static SettingsEvent* eventptr; /**< Pointer to the singleton instance of the SettingsEvent class. */
+     static HomeView* view; /**< Pointer to the HomeView object that the SettingsEvent class subscribes to. */
 };
 
 
--- gui/settings.h
+++ gui/settings.h
@@ -1,34 +1,95 @@
+/**
+ * @file Settings.h
+ * @author Nick
+ * @brief Defines the Settings class which manages application settings.
+ */
+
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
 #include <gtk/gtk.h>
 
+/**
+ * @class Settings
+ *
+ * @brief Manages application settings.
+ */
 class Settings {
 public:
+    /**
+     * @brief Returns a pointer to the Settings instance.
+     *
+     * @return Pointer to the Settings instance.
+     */
     static Settings* getInstance();
-
+    /**
+     * @brief Returns the background interval.
+     *
+     * @return The background interval.
+     */
 		int getBackgroundInterval();
-
+    /**
+     * @brief Callback function for increment button click.
+     *
+     * @param button The button that was clicked.
+     * @param user_data User-defined data.
+     */
     void on_increment_clicked(GtkButton* button, gpointer user_data);
+        /**
+     * @brief Callback function for decrement button click.
+     *
+     * @param button The button that was clicked.
+     * @param user_data User-defined data.
+     */
     void on_decrement_clicked(GtkButton* button, gpointer user_data);
+        /**
+     * @brief Saves the settings to the configuration file.
+     *
+     * @param widget The widget that triggered the signal.
+     * @param data User-defined data.
+     */
     void save_button(GtkWidget* widget, gpointer data);
+        /**
+     * @brief Closes the settings window.
+     *
+     * @param widget The widget that triggered the signal.
+     * @param data User-defined data.
+     */
     void closeSettings(GtkWidget* widget, gpointer data);
+    
+    /**
+     * @brief Saves the settings and closes the settings window.
+     *
+     * @param widget The widget that triggered the signal.
+     * @param data User-defined data.
+     */
     void saveSettings(GtkWidget* widget, gpointer data);
+    
+    /**
+     * @brief Opens the settings window.
+     */
     void open_settings_window();
 
 private:
-    Settings();  // Make constructor private
-    ~Settings(); // Make destructor private
-    static Settings* settingsptr;
-    GtkWidget* back_label;
-    GtkWidget* name_field;
-    GtkWidget* name_label;
-    GtkWidget* inc_button;
-    GtkWidget* dec_button;
-    GtkWidget* button;
-    bool Open;
-    int back_time;
-    GSettings* settings;
+    /**
+     * @brief Private constructor to prevent creation of Settings instances.
+     */
+    Settings();
+
+    /**
+     * @brief Private destructor to prevent destruction of Settings instances.
+     */
+    ~Settings();
+    static Settings* settingsptr; /**< Pointer to the Settings instance. */
+    GtkWidget* back_label; /**< Label widget for the background setting. */
+    GtkWidget* name_field; /**< Entry widget for the user name setting. */
+    GtkWidget* name_label; /**< Label widget for the user name setting. */
+    GtkWidget* inc_button; /**< Button widget for incrementing the counter. */
+    GtkWidget* dec_button; /**< Button widget for decrementing the counter. */
+    GtkWidget* button; /**< Button widget for saving the settings. */
+    bool Open; /**< Flag to indicate whether the settings window is open or not. */
+    int back_time; /**< The time in seconds for the background to change. */
+    GSettings* settings; /**< The GSettings object for storing the settings. */
 };
 
 #endif // SETTINGS_H
