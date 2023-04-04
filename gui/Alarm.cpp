47a030c3a2aec8caf1008495b2e538d4764f88d3
Mariam <malabi@uwo.ca>
2023-04-03 22:13:58 -0400

    doxygen comments added to Alarm.cpp

--- gui/Alarm.cpp
+++ gui/Alarm.cpp
@@ -23,20 +23,31 @@ int Alarm::getMinute() {
     return minute;
 }
 /**
- * @brief Get the minute of the alarm object
- * @return the minute variable
+ * @brief Get the hour of the alarm object
+ * @return the hour variable
  */
 int Alarm::getHour() {
     return hour;
 }
 
+/**
+ * @brief Set a new minute
+ * @param newMin. Set alarm to a new minute
+ */
 void Alarm::setMinute(int newMin) {
     minute = newMin;
 }
+/**
+ * @brief Set a new hour
+ * @param newH. Set alarm to a nee hour
+ */
 void Alarm::setHour(int newH) {
     hour = newH;
 }
-
+/**
+ * @brief set the alarm date
+ * @param alarm contains the date of the alarm
+ */
 void Alarm::setNewAlarm(GDateTime* alarm) {
     this->alarm = alarm;
 
