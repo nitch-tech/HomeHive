38c21698879951a2098eaf9c36bbc2e82450cb6b
Mariam <malabi@uwo.ca>
2023-04-03 22:51:22 -0400

    doxygen comments added to GuiComponent.cpp

--- gui/components/GuiComponent.cpp
+++ gui/components/GuiComponent.cpp
@@ -2,19 +2,33 @@
 // Created by dave on 03/04/23.
 //
 
+/**
+ * @brief Create a GuiComponent
+ */
+
 #include "GuiComponent.h"
 
-
+/**
+ * @brief GuiComponent constructor
+ */
 GuiComponent::GuiComponent() {
 }
-
+/**
+ * @brief GuiComponent deconstructor
+ */
 GuiComponent::~GuiComponent() {
 }
-
+/**
+ * set the grid
+ * @param parentGrid
+ */
 void GuiComponent::setParentGrid(GtkGrid* parentGrid) {
 	this->parentGrid = parentGrid;
 }
-
+/**
+ * Get the container
+ * @return GtkBox object
+ */
 GtkBox *GuiComponent::getContainer() {
 	return this->container;
 }
